a5a359a945f2aaaba3e598fa0c074ea80244d73a
StromAI <albstein2@gmail.com>
2015-01-31 15:07:18 +0800

    add host byte order program

---
+++ Unix_Network_Learn/SimpleDemo/byteorder.c
@@ -0,0 +1,31 @@
+/**
+ *	byteorder.c
+ *	Sliencer
+ *	1:31:2015
+ *	Such program will show us the host byter order
+ */ 
+
+#include "../unp.h"
+
+int
+main(int argc, char **argv)
+{
+	union {
+		short s;
+		char c[sizeof(short)];
+	} un;
+	un.s = 0x0102;
+	printf("%s:", CPU_VENDOR_OS);
+	if (sizeof(short) == 2)
+	{
+		if (un.c[0] == 1 && un.c[1] == 2)
+			printf("big-endian\n");
+		else if (un.c[0] == 2 && un.c[1] == 1)
+			printf("little-endian\n");
+		else
+			printf("unknown\n");
+	}
+	else
+		printf("sizeof(short) = %d\n", sizeof(short));
+	exit(0);
+}
